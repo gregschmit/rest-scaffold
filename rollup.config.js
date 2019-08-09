693865e80fe0b567058a2009ec3aa20d256a5b48
Gregory N. Schmit <schmitgreg@gmail.com>
2019-08-08 21:37:11 -0500

    add banner to rollup which includes version, and ensure that npm builds before publishing

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
 import buble from 'rollup-plugin-buble';
+import { version } from './package.json';
+
+var banner = `
+/**
+ * rest-scaffold.js version ${version}
+ * MIT License, Copyright (c) 2019 Gregory N. Schmit
+ */
+`;
 
 export default {
   input: "src/main.js",
   output: {
-    format: "umd",
+    banner: banner.trim(),
     file: "dist/rest-scaffold.js",
+    format: "umd",
     name: "rest-scaffold"
   },
   plugins: [buble()]
